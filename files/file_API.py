47fb020747612636c510788a407d262a908b7a35
Marat <maratsafin.let@gmail.com>
2023-06-08 16:55:54 +0200

    in file.api add get_from_accessible, get_all, get_from_common, get_from_favorite, rename_folder, move_to_folder, delete_list

--- files/file_API.py
+++ files/file_API.py
@@ -60,20 +60,109 @@ class Files:
         print(f"Создание папки в родительской папке id={folder_id} с кодом:", response.status_code)
         print(data)
 
-    def get_all_from_my(self):
+    def get_from_my_chapter(self) -> None:
         """
-        Получение всех файлов и папок в разделе Мои документы GET api/2.0/files/@my
+        Получение всех файлов и папок из раздела «Мои документы» GET api/2.0/files/@my
         :return: None
         """
         url_get_in_my = self.url_API_files + '/@my'
         response = requests.get(url_get_in_my, headers=self.headers)
         date = response.json()
 
-        print(f"Получение содержимого Мои документы с кодом:", response.status_code)
+        print(f"Получение содержимого «Мои документы» с кодом:", response.status_code)
         print(date)
+
+    def get_from_accessible_chapter(self) -> None:
+        """
+        Получение всех файлов и папок из раздела «Доступно для меня» GET api/2.0/files/@share
+        :return: None
+        """
+        url_get_in_common = self.url_API_files + '/@share?searchInContent=True&withSubfolders=True'
+        response = requests.get(url_get_in_common, headers=self.headers)
+        data = response.json()
+
+        print(f"Получение содержимого «Доступно для меня» с кодом:", response.status_code)
+        print(data)
+
+    def get_all_chapters(self) -> None:
+        """
+        Получение содерживого всех разделов GET api/2.0/files/@root
+        :return: None
+        """
+        url_get_all_chapters = self.url_API_files + '/@root?searchInContent=True&withSubfolders=True&withoutTrash=True&withoutAdditionalFolder=True'
+        response = requests.get(url_get_all_chapters, headers=self.headers)
+        data = response.json()
+
+        print(f"Получение содержимого всех разделов с кодом:", response.status_code)
+        print(data)
+
+    def get_from_common_chapter(self) -> None:
+        """
+        Получение списка файлов и папок, находящихся в разделе «Общие» GET api/2.0/files/@common
+        :return: None
+        """
+        url_get_from_common = self.url_API_files + '/@common?searchInContent=True&withSubfolders=True'
+        response = requests.get(url_get_from_common, headers=self.headers)
+        data = response.json()
+
+        print(f"Получение содержимого «Общие» с кодом:", response.status_code)
+        print(data)
+
+    def get_from_favorite_chapter(self) -> None:
+        """
+        Получение файлов и папок раздела «Избранное» GET api/2.0/files/@favorites
+        :return: None
+        """
+        url_get_from_favorite = self.url_API_files + '/@favorites?searchInContent=True&withSubfolders=True'
+        response = requests.get(url_get_from_favorite, headers=self.headers)
+        data = response.json()
+
+        print(f"Получение содержимого «Избранное» с кодом:", response.status_code)
+        print(data)
+
+    def rename_folder(self, data_rename_folder: dict, folder_id: int) -> None:
+        """
+        Переименование папки PUT api/2.0/files/folder/{folderId}
+        :param data_rename_folder: новое название папки
+        :param folder_id: идентификатор переименуемой папки
+        :return: None
+        """
+        url_rename_folder = self.url_API_files + f'/folder/{folder_id}'
+        response = requests.put(url_rename_folder, json=data_rename_folder, headers=self.headers)
+        data = response.json()
+
+        print(f"Переименование папки id={folder_id} с кодом:", response.status_code)
+        print(data)
+
+    def move_to_folder(self, data_move_file: dict) -> None:
+        """
+        Перемещение файлов и папок в указанную папку PUT api/2.0/files/fileops/move
+        :param data_move_file: данные о перемещаемых папках и файлах и папке назначения
+        :return: None
+        """
+        url_move_file = self.url_API_files + '/fileops/move'
+        response = requests.put(url_move_file, json=data_move_file, headers=self.headers)
+        data = response.json()
+
+        print(f"Перемещение в папку с кодом:", response.status_code)
+        print(data)
+
+    def delete_list(self, data_delete: dict) -> None:
+        """
+        Удаление папок и файлов PUT api/2.0/files/fileops/delete
+        :param data_delete: данные об удаляемых файлах и папках
+        :return: None
+        """
+        url_delete_list = self.url_API_files + '/fileops/delete'
+        response = requests.put(url_delete_list, json=data_delete, headers=self.headers)
+        data = response.json()
+
+        print(f"Удаление с кодом:", response.status_code)
+        print(data)
 
 
 if __name__ == "__main__":
+
     """
 **************************************************************************************************
     Данные для авторизации (при запуске подставлять свои значения)
@@ -92,5 +181,48 @@ if __name__ == "__main__":
     data_create_folder = {
         "title": "My folder2",
     }
+    """
+**************************************************************************************************
+    Данные для переименования папки
+**************************************************************************************************
+    """
+    data_rename_folder = {
+        "title": "Renamed folder"
+    }
+    """
+**************************************************************************************************
+    Данные для перемещения папок и файла в папку
+**************************************************************************************************
+      """
+    data_move_file = {
+        "destFolderId": "2",
+        "folderIds": [
+            "36",
+            "35",
+        ],
+        "fileIds": [
+        ],
+        "conflictResolveType": "Overwrite",
+        "deleteAfter": True
+    }
+    """
+**************************************************************************************************
+    Данные для удаления папок и файлов
+**************************************************************************************************
+      """
+    data_delete = {
+
+        "folderIds": [
+            "34",
+            "35",
+            "36"
+        ],
+        "fileIds": [
+
+        ],
+        "deleteAfter": True,
+        "immediately": True
+    }
 
     files = Files(url_auth, data_auth)
+    files.delete_list(data_delete)
