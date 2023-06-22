50804d7cca0c6ae3dc803d7fa20e3cd594d90b42
Marat <maratsafin.let@gmail.com>
2023-06-22 10:42:35 +0200

    finish settings

--- settings/settings_API.py
+++ settings/settings_API.py
@@ -42,6 +42,7 @@ class CommonSettings:
         response = requests.get(self.url_API_settings, headers=self.headers)
         data = response.json()
 
+        print(f"Получение настроек портала с кодом:", response.status_code)
         print(data)
 
     def email_activation(self) -> None:
@@ -53,6 +54,91 @@ class CommonSettings:
         response = requests.put(url_email_activation, headers=self.headers)
         data = response.json()
 
+        print(f"Получение настроек активации электронной почты с кодом:", response.status_code)
+        print(data)
+
+    def ip_restrictions(self) -> None:
+        """
+        Получение ограничений IP-портала GET api/2.0/settings/iprestrictions
+        :return: None
+        """
+        url_email_activation = self.url_API_settings + '/iprestrictions'
+        response = requests.get(url_email_activation, headers=self.headers)
+        data = response.json()
+
+        print(f"Получение ограничений IP-портала с кодом:", response.status_code)
+        print(data)
+
+    def get_smtp(self) -> None:
+        """
+        Получение настроек SMTP GET api/2.0/settings/smtp
+        :return: None
+        """
+        url_smtp = self.url_API_settings + '/smtp'
+        response = requests.get(url_smtp, headers=self.headers)
+        data = response.json()
+
+        print(f"Получение настроек SMTP с кодом:", response.status_code)
+        print(data)
+
+    def test_smtp(self) -> None:
+        """
+        Тестирование настроек SMTP GET api/2.0/settings/smtp/test
+        :return: None
+        """
+        url_test_smtp = self.url_API_settings + '/smtp/test'
+        response = requests.get(url_test_smtp, headers=self.headers)
+        data = response.json()
+
+        print(f"Тестирование SMTP с кодом:", response.status_code)
+        print(data)
+
+    def get_sso(self) -> None:
+        """
+        Получение настроек SSO api/2.0/settings/ssov2
+        :return: None
+        """
+        url_sso = self.url_API_settings + '/ssov2'
+        response = requests.get(url_sso, headers=self.headers)
+        data = response.json()
+
+        print(f"Получение настроек SSO с кодом:", response.status_code)
+        print(data)
+
+    def get_paasword_settings(self) -> None:
+        """
+        Получение настроек пароля к порталу GET api/2.0/settings/security/password
+        :return: None
+        """
+        url_get_password_settings = self.url_API_settings + '/security/password'
+        response = requests.get(url_get_password_settings, headers=self.headers)
+        data = response.json()
+
+        print(f"Получение настроек пароля к порталу с кодом:", response.status_code)
+        print(data)
+
+    def get_security_settings(self) -> None:
+        """
+        Получение настроек безопасности продукта GET api/2.0/settings/security
+        :return: None
+        """
+        url_get_security_settings = self.url_API_settings + '/security'
+        response = requests.get(url_get_security_settings, headers=self.headers)
+        data = response.json()
+
+        print(f"Получение настроек безопасности продукта с кодом:", response.status_code)
+        print(data)
+
+    def get_size_quota(self) -> None:
+        """
+        Получение квоты использования пространства GET api/2.0/settings/quota
+        :return: None
+        """
+        url_get_size_quota = self.url_API_settings + '/quota'
+        response = requests.get(url_get_size_quota, headers=self.headers)
+        data = response.json()
+
+        print(f"Получение квоты использования пространства с кодом:", response.status_code)
         print(data)
 
 
@@ -79,4 +165,4 @@ if __name__ == "__main__":
     }
 
     settings_ = CommonSettings(url_auth, data_auth)
-    settings_.email_activation()
+    settings_.get_size_quota()
